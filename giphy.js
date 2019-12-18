5d2c0944e025275655e11b15bc283e33538fbe59
ashya <ashyamanning@pursuit.org>
2019-12-17 20:56:57 -0500

    add showGif function

--- giphy.js
+++ giphy.js
@@ -5,21 +5,34 @@ document.addEventListener("DOMContentLoaded", () => {
     let input = document.querySelector("#userInput");
     let submit = document.querySelector("#submit");
     let content = document.querySelector(".content");
-    let limit = 10;
-    let search = "cats";
+   
     
     let h1 = document.createElement("h1");
 
-    form.addEventListener("Submit", () => {
-        giphySearch(search, limit);
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        giphySearch(input.value, select.value);
     })
+
+    select.addEventListener("change", (e) => {
+        select.value = e.target.value;
+    })
+
+    const showGif = (gifArr) => {
+        gifArr.forEach(gif => {
+            let image = document.createElement("img");
+            image.src = gif.images.downsized.url;
+            content.appendChild(image);
+        })
+    }
 
     const giphySearch = async (userInput, userLimit) => {
         try {
+            content.innerHTML = "";
             let res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${userInput}&limit=${userLimit}`)
-            // debugger
+            showGif(res.data.data);
         } catch (error) {
-            
+            console.log(error);
         }
     }
     
